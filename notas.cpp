21cb447d03a0cac9ece706e4b31f057f1a62771f
Your Name <you@example.com>
2021-07-27 19:43:40 -0300

    Ajuste 2

---
+++ notas.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float n;
+	cout<<"Digite uma nota"<<endl;
+	cin>>n;
+    	if (n<=7)
+	    {
+      		cout<<"Reprovado!"<<endl;
+        }
+	    if ((7<n)&&(n<10))
+	    {
+	     	cout<<"Aprovado!"<<endl;
+	    }
+	    if (n==10)
+	    {
+	    	cout<<"Aprovado com louvor!!"<<endl;
+
+
+	return 0;
+}
